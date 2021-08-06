20ed69d9490dad4b563a115a8f00ff6daadf78f1
eric tush <erotush77@gmail.com>
2021-08-06 21:22:10 +0300

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00194c",
+        secondary: "#eab200",
+        "primary-accent": "#014067",
+        "secondary-accent": "#a5a5a5",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
